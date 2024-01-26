159f4e3875ac4f94466dbb0d15f8f4535598f374
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 <officeight@naver.com>
2024-01-26 15:35:48 +0900

    test

---
+++ SpringDIProject_ex10/src/main/java/com/sist/dao/TestDAO.java
@@ -0,0 +1,8 @@
+package com.sist.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository("tDAO")
+public class TestDAO {
+	
+}
---
+++ SpringDIProject_ex10/src/main/java/com/sist/dao/TestVO.java
@@ -0,0 +1,37 @@
+package com.sist.dao;
+
+public class TestVO {
+	private int no,test1,test2,test3;
+	private String name;
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getTest1() {
+		return test1;
+	}
+	public void setTest1(int test1) {
+		this.test1 = test1;
+	}
+	public int getTest2() {
+		return test2;
+	}
+	public void setTest2(int test2) {
+		this.test2 = test2;
+	}
+	public int getTest3() {
+		return test3;
+	}
+	public void setTest3(int test3) {
+		this.test3 = test3;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
